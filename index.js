54648ac2e82da1be51b0ac7dfcc0037468b6fadb
Satyam Mishra <satyam.mishra975@gmail.com>
2020-12-16 11:12:01 +0530

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+function isNumber(val){
+    return !Number.isNaN(Number(val))
+}
+export function between(min, max,decimal_digits=0){
+    if(isNumber(min) && isNumber(max) && isNumber(decimal_digits)){
+        return Number((Math.random() * (max - min) + min).toFixed(decimal_digits))
+    }  
+}
